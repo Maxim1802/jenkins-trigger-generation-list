f222bf1c756c59371c8155a6b393413c89b336b3
Maxim Trogaev <moriarty18.02@gmail.com>
2023-02-20 15:22:20 +0000

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hi")
+import os
+
+def main():
+  jobs_json = os.environ["INPUT_JOBS_JSON"]
+  print(jobs_json)
+
+  print(f"::set-output name=build_url::{build_url}")
+  print(f"::notice title=build_url::{build_url}")
+
+if __name__ == "__main__":
+  main()
