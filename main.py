73519d4659f13e56e12c387ae5856c32782541a2
Maxim Trogaev <moriarty18.02@gmail.com>
2023-02-20 15:43:34 +0000

    init commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
   jobs_json = os.environ["INPUT_JOBS_JSON"]
   print(jobs_json)
 
+  build_url = jobs_json
+
   print(f"::set-output name=build_url::{build_url}")
   print(f"::notice title=build_url::{build_url}")
 
